1e759fd25230882d1b5e7c7e8652a8c96e55e37b
twead <45719524+twead@users.noreply.github.com>
2021-12-02 01:41:58 +0100

    addCategory template

--- ecommerce-ui/src/router/index.js
+++ ecommerce-ui/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import AddCategory from '../views/category/AddCategory.vue'
 
 const routes = [
   {
@@ -10,11 +12,13 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: About
+  },
+  {
+    path: '/admin/category/add',
+    name: 'AddCategory',
+    component: AddCategory
+  },
 ]
 
 const router = createRouter({
---
+++ ecommerce-ui/src/views/category/AddCategory.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h3 class="pt-3">Add Category</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form>
+          <div class="form-group">
+            <label> Name </label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label> Description </label>
+            <textarea type="text" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label> Image </label>
+            <input type="text" class="form-control" />
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div class="col-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
